1e28a527af745442958ad4cc0decae2bfcaba15a
Cetys <cetys@cetyss-mac.local>
2018-10-19 17:04:02 +0200

    Initial Commit

---
+++ Ahorcado1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ahorcado1
+//
+//  Created by Cetys on 19/10/18.
+//  Copyright © 2018 cetys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
