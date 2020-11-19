48f389de6c844645ecbd1f8b92d54ed9f37edf97
Cetys <cetys@cetyss-mac.local>
2020-11-19 19:42:56 +0100

    AHORCADO _ 2020 (NO FUNCIONA BIEN)

--- Ahorcado1/ViewController.swift
+++ Ahorcado1/ViewController.swift
@@ -10,9 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let palabraSecreta : String = "cetys"
+    
+    @IBOutlet var palabraOculta: UILabel!
+    
+    @IBAction func letraPulsada(_ sender: UIButton) {
+        let letra = Array((sender.currentTitle!).characters)[0]
+        print(letra);
+        chequeaLetra(letra: letra);
+    }
+    func chequeaLetra (letra : Character) ->Int{
+        var aciertos = 0
+        var auxiliar = ""
+        var posicion = 0
+        for p in palabraSecreta{
+            if p == letra{
+                print("SI ESTA")
+                aciertos = aciertos + 1
+                auxiliar.append(letra)
+            }
+            else{
+                auxiliar.append(Array((palabraOculta.text!).characters)[posicion])
+            }
+            auxiliar = auxiliar + " "
+            posicion = posicion + 1
+        }
+        
+        palabraOculta.text  = auxiliar
+        return aciertos
+    }
+    
+    func poneGuionesEnLabel(){
+        var auxiliar = ""
+        for _ in palabraSecreta.characters{
+            auxiliar + "_ "
+        }
+        palabraOculta.text = auxiliar
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
